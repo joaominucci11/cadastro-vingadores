6d906f77b3f4d9d96ef491d3203dc4776ab671aa
João Pedro Minucci Regueira <159261517+joaominucci11@users.noreply.github.com>
2024-12-04 13:08:18 -0300

    Add files via upload

--- model/database.py
+++ model/database.py
@@ -1,13 +1,15 @@
 import mysql.connector
 from mysql.connector import Error
+from dotenv import load_dotenv
+from os import getenv
 
-class Database: 
+class Database:
     def __init__(self):
-        self.host = 'localhost'
-        self.user = 'root'
-        self.password = 'admin'
-        self.database = 'vingadores'
-
+        load_dotenv() # carregando as variáveis de ambiente
+        self.host = getenv('BD_HOST')
+        self.user = getenv('BD_USER')
+        self.password = getenv('BD_PSWD')
+        self.database = getenv('BD_DATABASE')
 
     def connect(self):
         try:
@@ -18,13 +20,28 @@ class Database:
                 database = self.database
             )
             self.cursor = self.connection.cursor()
-            print('Conexão com o banco de dados realizadas com sucesso')
+            print('Conexão com o banco de dados realizada com sucesso')
         except Error as e:
             print(f'Erro: {e}')
 
-Database().connect()
-#db = Database()
-#db.connect()
+    def disconnect(self):
+        self.connection.close()
+        print('Conexão com o banco de dados encerrada com sucesso')
 
+    def execute_query(self, query, values=None):
+        try:
+            self.cursor.execute(query, values)
+            self.connection.commit()
+            print('Query executada com sucesso')
+            return self.cursor
+        except Error as e:
+            print(f'Erro: {e}')
+            return None
 
-        
+    def select(self, query):
+        try: 
+            self.cursor.execute(query)
+            return self.cursor.fetchall()
+        except Error as e:
+            print(f'Erro: {e}')
+            return None
